8d9f3657b96fbde8d99a37a404aa7e7cab310ef1
alex.misyuk1@gmail.com <al46212542>
2016-12-25 18:01:05 +0200

    Add array

--- src/com/urise/webapp/storage/AbstractArrayStorage.java
+++ src/com/urise/webapp/storage/AbstractArrayStorage.java
@@ -18,12 +18,60 @@ public abstract class AbstractArrayStorage implements Storage {
 
     public Resume get(String uuid) {
         int index = getIndex(uuid);
-        if(index == -1) {
+        if(index < 0) {
             System.out.println("Resume" + uuid + "not exist");
             return null;
         }
         return storage[index];
     }
+
+    public void clear() {
+        Arrays.fill(storage,0,size,null);
+        size=0;
+    }
+
+
+    public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+
+        if(index == -1){
+            System.out.println("Resume"+ r.getUuid()+ "not exist");
+        }
+        else {
+            storage[index] = r;
+        }
+
+    }
+    public Resume[] getAll(){
+        return Arrays.copyOfRange(storage,0,size);
+    }
+
+    public void save(Resume r) {
+        int index = getIndex(r.getUuid());
+        if(index > 0){
+            System.out.println("Resume"+ r.getUuid()+ "already exist");
+        }else if(size == storage.length){
+            System.out.println("Storage overflow");
+        } else{
+            insertElement(r, index);
+            size++;
+        }
+    }
+
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if(index <0){
+            System.out.println("Resume"+ uuid+ "not exist");
+        }else{
+            fillDeletedElement(index);
+            storage[size - 1]= null;
+            size--;
+        }
+    }
+
+    protected abstract void fillDeletedElement(int index);
+
+    protected abstract void insertElement(Resume r, int index);
 
     protected abstract int getIndex(String uuid);
 
--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -9,30 +9,24 @@ import java.util.Arrays;
  */
 public class SortedArrayStorage extends AbstractArrayStorage{
 
-    @Override
-    public void clear() {
 
+
+    @Override
+    protected void fillDeletedElement(int index) {
+        int numMoved = size - index -1;
+        if(numMoved > 0){
+            System.arraycopy(storage , index +1, storage, index, numMoved);
+        }
     }
 
     @Override
-    public void update(Resume r) {
-
+    protected void insertElement(Resume r, int index) {
+        int insertIdx = -index - 1;
+        System.arraycopy(storage , insertIdx,storage,insertIdx+1,size-insertIdx);
+        storage[insertIdx] = r;
     }
 
-    @Override
-    public void save(Resume r) {
 
-    }
-
-    @Override
-    public void delete(String uuid) {
-
-    }
-
-    @Override
-    public Resume[] getAll() {
-        return new Resume[0];
-    }
 
     @Override
     protected int getIndex(String uuid) {
